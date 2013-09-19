e2794456c7acca00001831d5520f3c5cdfcb310f
ogranada <oscar.andres.granada@gmail.com>
2013-09-19 14:11:29 -0500

    Notifications Helper Added

---
+++ src/com/framework/leopardus/utils/Notifications.java
@@ -0,0 +1,71 @@
+package com.framework.leopardus.utils;
+
+import java.util.Random;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+
+public class Notifications {
+
+	public static int getId() {
+		return (new Random()).nextInt();
+	}
+
+	@SuppressWarnings("rawtypes")
+	public static int sendNotification(Context ctx, int smallIcon,
+			String titile, String content, Class clazz) {
+		int id = getId();
+		updateNotification(ctx, id, smallIcon, titile, content, clazz, true, null);
+		return id;
+	}
+
+	@SuppressWarnings("rawtypes")
+	public static void updateNotification(Context ctx, int id, int smallIcon,
+			String titile, String content, Class clazz) {
+		updateNotification(ctx, id, smallIcon, titile, content, clazz, true, null);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public static int sendNotification(Context ctx, int smallIcon,
+			String titile, String content, Class clazz, boolean autoCancel,
+			String[] multiLines) {
+		int id = getId();
+		updateNotification(ctx, id, smallIcon, titile, content, clazz, autoCancel, multiLines);
+		return id;
+	}
+
+	@SuppressWarnings("rawtypes")
+	public static void updateNotification(Context ctx, int id, int smallIcon,
+			String titile, String content, Class clazz, boolean autoCancel,
+			String[] multiLines) {
+		NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
+				ctx).setSmallIcon(smallIcon).setContentTitle(titile)
+				.setContentText(content);
+		TaskStackBuilder stackBuilder = TaskStackBuilder.create(ctx);
+		if (clazz != null) {
+			Intent resultIntent = new Intent(ctx, clazz);
+			stackBuilder.addParentStack(clazz);
+			stackBuilder.addNextIntent(resultIntent);
+		}
+		if(multiLines!=null && multiLines.length>0){
+			NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+			inboxStyle.setBigContentTitle(content);
+			for (int i = 0; i < multiLines.length; i++) {
+				inboxStyle.addLine(multiLines[i]);
+			}
+			mBuilder.setStyle(inboxStyle);
+		}
+		mBuilder.setAutoCancel(autoCancel);
+		PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0,
+				PendingIntent.FLAG_UPDATE_CURRENT);
+		mBuilder.setContentIntent(resultPendingIntent);
+		NotificationManager mNotificationManager = (NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+		mBuilder.setAutoCancel(true);
+		mNotificationManager.notify(id, mBuilder.build());
+	}
+
+}
